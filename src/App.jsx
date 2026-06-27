b6596c8a956d4b9930bdb48318615e9fa6ae2502
aazimsaeed <syedaazim9149@gmail.com>
2026-06-27 14:21:46 +0530

    config location fixed in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { API_BASE } from '../config';
+import { API_BASE } from './config';
 // Import Pages
 import LandingPage from './pages/LandingPage';
 import AuthPage from './pages/AuthPage';
